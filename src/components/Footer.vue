df1fae00d05bc955aed91afc19e75d1fcfb9eb3d
Babatunde Victor <31570907+ojobabs@users.noreply.github.com>
2022-02-28 23:40:39 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,14 +4,8 @@
   <footer
     class="h-32 bottom-0 inset-x-0 p-4 z-10 flex justify-center space-x-4 theme-main transition-colors duration-250"
   >
-    <span class="text-sm opacity-75 text-center"
-      ><a
-        target="_blank"
-        href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-        class="text-green-500"
-        >CC BY-NC-SA 4.0</a
-      >
-      2022 © Babatunde Ojo</span
+    <span class="text-sm opacity-75 text-center">
+       © 2022 | Babatunde Ojo</span
     >
   </footer>
 </template>
